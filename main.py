cb6df0db40705e4bd92eaf755f81447c457ded2b
Wesley Petit <wesley.petit.lemoine@gmail.com>
2023-07-21 18:06:45 +0200

    Add basic main checking a default sun position and print result

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import SunlightConverter
 
 
 def main():
+    print("Load triangles from tileset...")
     triangle_soup = SunlightConverter.get_triangle_soup_from_tileset()
+    print(f"Successfully load {len(triangle_soup)} triangles !")
 
-    origin = pySunlight.Vec3d(0, 0, 0)
-    direction = pySunlight.Vec3d(0, 10, 0)
-    ray = pySunlight.Ray(origin, direction)
+    sunDirection = pySunlight.Vec3d(0, 0, 0)
 
-    pySunlight.checkIntersectionWith(ray, triangle_soup)
+    for triangle in triangle_soup:
+        ray = pySunlight.constructRay(triangle, sunDirection)
+        triangleRayHits = pySunlight.checkIntersectionWith(ray, triangle_soup)
 
+        if 0 < len(triangleRayHits):
+            print(f"Triangle {triangle.getId()} is blocked by {triangleRayHits[0].triangle.getId()}")
+
+        # else:
+        #     print(f"Triangle {triangle.getId()} is in light")
 
 if __name__ == '__main__':
     main()
