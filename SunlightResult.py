adbb88aa18a3b8702513d12cca525e0563b47919
Wesley Petit <wesley.petit.lemoine@gmail.com>
2023-07-21 18:27:44 +0200

    Add SunlightResult class and display memory size of triangles

---
+++ SunlightResult.py
@@ -0,0 +1,7 @@
+
+
+class SunlightResult:
+    def __init__(self, date: str, bLighted: bool, blockerId: str):
+        self.date = date
+        self.bLighted = bLighted
+        self.blockerId = blockerId
--- main.py
+++ main.py
@@ -1,23 +1,38 @@
+# Sunlight wrapped in python
 import pySunlight
+# Convert py3DTilers type in Sunlight types
 import SunlightConverter
-
+from SunlightResult import SunlightResult
+import sys
 
 def main():
     print("Load triangles from tileset...")
     triangle_soup = SunlightConverter.get_triangle_soup_from_tileset()
     print(f"Successfully load {len(triangle_soup)} triangles !")
 
+    # Convert octet to Mega octet
+    print(f"We are using {len(triangle_soup) * sys.getsizeof(pySunlight.Triangle) / 64} Mo for our triangles.")
+
+    results = list()
+
     sunDirection = pySunlight.Vec3d(0, 0, 0)
+    date = "2016-01-01"
 
     for triangle in triangle_soup:
+
         ray = pySunlight.constructRay(triangle, sunDirection)
+
+        # Sort result by impact distance (from near to far)
         triangleRayHits = pySunlight.checkIntersectionWith(ray, triangle_soup)
 
         if 0 < len(triangleRayHits):
-            print(f"Triangle {triangle.getId()} is blocked by {triangleRayHits[0].triangle.getId()}")
+            # We consider the first triangle to be blocking
+            nearestHitTriangle = triangleRayHits[0].triangle
+            results.append(SunlightResult(date, False, nearestHitTriangle.getId()))
 
-        # else:
-        #     print(f"Triangle {triangle.getId()} is in light")
+        # Triangle is in plain sunlight
+        else:
+            results.append(SunlightResult(date, True, ""))
 
 if __name__ == '__main__':
     main()
